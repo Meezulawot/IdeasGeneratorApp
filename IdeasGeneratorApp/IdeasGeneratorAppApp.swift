18e428342696e693c4ae481f265bfddbf584d5c0
Meezu Lawot <meezu.lawot@swifttech.com.np>
2024-09-01 10:55:00 +0545

    Initial Commit

---
+++ IdeasGeneratorApp/IdeasGeneratorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeasGeneratorAppApp.swift
+//  IdeasGeneratorApp
+//
+//  Created by Meezu Lawot on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct IdeasGeneratorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
